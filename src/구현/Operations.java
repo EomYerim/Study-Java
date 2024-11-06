6bcdfdafc7f4ba8c2a1a75b76f72ad7794de0cca
Eom-Ye-rim <dpfladlwkdrn@naver.com>
2024-11-06 16:51:05 +0900

    [update] 백준 10869 문제

---
+++ src/구현/Operations.java
@@ -0,0 +1,18 @@
+package 구현;
+
+import java.util.Scanner;
+
+public class Operations {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a;
+		int b;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
